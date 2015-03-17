06cdbe856e18d43ee6a935f59646bd5bc22f8d6a
RnzTx <karadkar.rohit@gmail.com>
2015-03-17 22:16:53 +0530

    optimised menu.css

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
    <div id="wrapper">
       <nav>
         <ul class="menu">
-          <li class="home current"><a href="index.php"><span>Home</span></a></li>
+          <li><a href="index.php"><span>Home</span></a></li>
           <li><a href="topicList.php"><span>Topics</span></a></li>
           <li><a href="#"><span>recent </span></a></li>
           <li><a href="#"><span>About</span></a></li>
