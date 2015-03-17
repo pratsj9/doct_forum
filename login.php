6dcf27cd8e2e9675212f7709ffc21e0b8c55a5fc
RnzTx <karadkar.rohit@gmail.com>
2015-03-17 20:22:44 +0530

    new file:css/auth.css - stylesheet for login and registration; new file: login.php, register.php; modified: menu.php - added login & register page;

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <?php include 'menu.php'?>
+  <title>User login</title>
+  
+  <link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="html/css" href="css/auth.css" >
+</head>
+<body>
+  <div class="box">
+    <div class="content">
+      <h1><strong>User Login</strong></h1>
+      <input class="field" type="text" name="email" placeholder="email id"><br>
+      <input class="field" type="password" name="password" placeholder="password"><br>
+      <input class="btn" type="submit" value="Login">  
+    </div>
+  </div>
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -8,17 +8,13 @@
         <ul class="menu">
           <li class="home current"><a href="index.php"><span>Home</span></a></li>
           <li><a href="topicList.php"><span>Topics</span></a></li>
-          <li><a href="#"><span>recent </span></a>
-            <ul class="sub-menu">
-              <li><a href="#">Sub-menu 1</a></li>
-              <li><a href="#">Sub-menu 2</a></li>
-              <li><a href="#">Sub-menu 3</a></li>
-            </ul>
-          </li>
+          <li><a href="#"><span>recent </span></a></li>
           <li><a href="#"><span>About</span></a></li>
           <li><a href="#"><span>Contacts</span></a></li>
           <li><a href="#"><span>User</span></a>
             <ul class="sub-menu">
+              <li><a href="login.php">Login</a></li>
+              <li><a href="register.php">Register</a></li>
               <li><a href="#">Logout</a></li>
             </ul>
           </li>
