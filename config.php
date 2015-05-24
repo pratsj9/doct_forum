7556d6413e4ef22fc22649cf7d4e3c22ae286429
RnzTx <karadkar.rohit@gmail.com>
2015-05-24 15:10:49 +0530

    simplify

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$currency = 'INR'; //Currency sumbol or code
-
-$host = "localhost";
-$username = "root";
-$password = "precious";
-$database = "forum";
-$mysqli = new mysqli($host, $username, $password, $database);
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-include 'include/core.php';
-redirect("index.php");
-?>
